82be90916c66518e0afe2eedd6b7c1d585a493a0
Nurul Islam <nurulbdpersonal@gmail.com>
2022-04-15 17:42:36 +0600

    env serup for firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4S2qXXWrEZcHOCnokiLpMzeXycH2B3qQ",
-  authDomain: "westin-hotel.firebaseapp.com",
-  projectId: "westin-hotel",
-  storageBucket: "westin-hotel.appspot.com",
-  messagingSenderId: "62751670392",
-  appId: "1:62751670392:web:b62cc9ff2f09af3a6d2d8d"
-};
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+  const auth = getAuth(app);
 
-export default auth;
+  export default auth;
